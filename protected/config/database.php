43e3b108d0cfc50347b178060c38d9a0083b13d9
Rajoooooo <rajocastro1211@gmail.com>
2025-03-17 16:13:12 +0800

    updated database

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=blog_db',
+	'connectionString' => 'mysql:host=localhost;dbname=yii_blog_db',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
